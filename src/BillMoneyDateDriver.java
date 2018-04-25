951339d72f73331081aaabb48c25949da143d21c
Ian Bryan <incomplete@users.noreply.github.com>
2018-04-25 12:59:59 -0700

    Money Class nearly done

--- src/BillMoneyDateDriver.java
+++ src/BillMoneyDateDriver.java
@@ -13,32 +13,32 @@ public class BillMoneyDateDriver
     public static void main(String[] args)
     {
         //Construct some money
-        Money money1 = new Money(10);
-        Money money2 = new Money(money1);
-        money1.setMoney(30,50);
+        Money money1 = new Money(10, 50);
+        //Money money2 = new Money(money1);
+        //money1.setMoney(30,50);
         //TODO: do more functional exercises with the money class
 	    
 	    
         System.out.println("Money objects output:");
         System.out.println(money1);
-        System.out.println(money2);
+        //System.out.println(money2);
 	
 	
         //Construct some bills
-        Money amount = new Money(20);
-        Date dueDate = new Date(4, 30, 2007);
-        Bill bill1 = new Bill(amount, dueDate, "The phone company");
-       
-        Bill bill2 = new Bill(bill1);
-        bill2.setDueDate(new Date(5, 30, 2007));
-        amount.setMoney(31, 99);
-        dueDate.setDay(29);
-        Bill bill3 = new Bill(amount, dueDate, "The record company");
-        
-        System.out.println("Bill objects output:");
-        System.out.println(bill1);
-        System.out.println(bill2);
-        System.out.println(bill3);
+//        Money amount = new Money(20);
+//        Date dueDate = new Date(4, 30, 2007);
+//        Bill bill1 = new Bill(amount, dueDate, "The phone company");
+//       
+//        Bill bill2 = new Bill(bill1);
+//        bill2.setDueDate(new Date(5, 30, 2007));
+//        amount.setMoney(31, 99);
+//        dueDate.setDay(29);
+//        Bill bill3 = new Bill(amount, dueDate, "The record company");
+//        
+//        System.out.println("Bill objects output:");
+//        System.out.println(bill1);
+//        System.out.println(bill2);
+//        System.out.println(bill3);
         
     }
 }
--- src/Money.java
+++ src/Money.java
@@ -1,4 +1,49 @@
 
 public class Money {
+	
+	//class variables
+	private int dollars;
+	private int cents;
+	private double newDollars;
+	private double centsInDollars;
+	private int centsRemaining;
+	
+	public Money(int dols){
+		this.dollars = dols;
+		this.cents = 0;
+	}
+	
+	public Money(int dols, int cents){
+		this.dollars = dols;
+		this.cents = cents;
+		getMoney();
+	}
+	
+	public Money(Money other){
+		this.dollars = other.dollars;
+		this.cents = other.cents;
+	}
+	
+	public double add(int dollars, int cents){
+		//da fuck do i do here
+	}
 
+	public void setMoney(int dollars, int cents){
+		//dunno
+	}
+	public double getMoney(){
+		//return a double
+	}
+	
+	public double getDollars(){
+		return dollars;
+	}
+	
+	public int getCents(){
+		return cents;
+	}
+	
+	public String toString(){
+		return "$" + this.getMoney();
+	}
 }
