a9f101f6b59a7336cb9eaad165497ba1de673ce5
Ian B <ianbryanz@gmail.com>
2018-10-25 16:12:56 -0700

    Revert "Pulling to Windows Build"
    This reverts commit 0e1b21b4e7d26cc42ce38062fde236c5574cff3a.

--- src/BillMoneyDateDriver.java
+++ src/BillMoneyDateDriver.java
@@ -15,6 +15,7 @@ public class BillMoneyDateDriver
 	 */
 	public static void main(String[] args)
 	{
+<<<<<<< HEAD
 		/*
 		 * Testing the Money Class
 		 * Money1 | Money2 | Money3
@@ -25,6 +26,17 @@ public class BillMoneyDateDriver
 		Money money1 = new Money(10);
 		Money money2 = new Money(10,101);
 		Money money3 = money1;
+=======
+		//Construct some money
+		//Money money1 = new Money(10);
+		//Money money1 = new Money(10,1);
+		//Money money1 = new Money(10,99);
+		//Money money1 = new Money(10,101);
+		//Money money2 = new Money(12);
+
+		//useful for when needed but using two objects to play with seemed to work out alright.
+		//Money money3 = new Money(11);
+>>>>>>> parent of 0e1b21b... Pulling to Windows Build
 
 		//money1.setMoney(30,50);
 
@@ -50,14 +62,14 @@ public class BillMoneyDateDriver
 		/****************************************************/
 		/* Testing the Date class */
 		/****************************************************/
-//		Date date1 = new Date(1,1,2016);
-//		Date date2 = new Date(1,1,2016);
-//
-//		assert(date1 != null);
-//		assert(date2 != null);
-//
-//		System.out.println(date1);
-//		System.out.println(date2);
+		Date date1 = new Date(1,1,2016);
+		Date date2 = new Date(1,1,2016);
+
+		assert(date1 != null);
+		assert(date2 != null);
+
+		System.out.println(date1);
+		System.out.println(date2);
 
 		//checking the isAfter functionality
 		//comments on method functionality in Date class
@@ -74,43 +86,43 @@ public class BillMoneyDateDriver
 		/* Testing the Bill class							 */
 		/*****************************************************/
 		//Construct some bills
-//		Money amount = new Money(50);
-//		Date dueDate = new Date(4, 30, 2017);
-//		Date datePaid = new Date(5, 15, 2017);
+		Money amount = new Money(50);
+		Date dueDate = new Date(4, 30, 2017);
+		Date datePaid = new Date(5, 15, 2017);
 
 		//instantiating a new object bill1 with values
 		//from the Money object 'amount' and the Date object
 		//'dueDate'. Then copy it using the copy ctor in
 		//the Bill class.
-//		Bill bill1 = new Bill(amount, dueDate, "The phone company");
-//		Bill bill2 = new Bill(bill1);
-//
-//		System.out.println("----------Bill 1------------");
-//		System.out.println(bill1);
-//		System.out.println("----------Bill 2------------");
-//		System.out.println(bill2);
+		Bill bill1 = new Bill(amount, dueDate, "The phone company");
+		Bill bill2 = new Bill(bill1);
+
+		System.out.println("----------Bill 1------------");
+		System.out.println(bill1);
+		System.out.println("----------Bill 2------------");
+		System.out.println(bill2);
 		
 		//checking the equals method
 		//will be reused a few times to 
 		//keep track of the bills.
-//		System.out.println("---------------------------");
-//		System.out.println("Bill has been not updated?: " + bill1.equals(bill2));
+		System.out.println("---------------------------");
+		System.out.println("Bill has been not updated?: " + bill1.equals(bill2));
 
 		//using the setDueDate method that resets the
 		//dueDate value on the conditions of the isPaid
 		//method in the Bill class.
-//		bill1.setDueDate(new Date(5, 30, 2017));
-//		System.out.println("----------Bill 1------------");
-//		System.out.println(bill1);
-//		System.out.println("---------------------------");
-//		System.out.println("Bill has been not updated?: " + bill1.equals(bill2));
-//		
-//		//checking the setPaid method
-//		//and setting the amount to 0 to show this.
-//		Money newAmount = new Money(0);
-//		bill1.setAmount(newAmount);
-//		bill1.setPaid(datePaid);
-//		System.out.println("----------Bill 1------------");
-//		System.out.println(bill1);
+		bill1.setDueDate(new Date(5, 30, 2017));
+		System.out.println("----------Bill 1------------");
+		System.out.println(bill1);
+		System.out.println("---------------------------");
+		System.out.println("Bill has been not updated?: " + bill1.equals(bill2));
+		
+		//checking the setPaid method
+		//and setting the amount to 0 to show this.
+		Money newAmount = new Money(0);
+		bill1.setAmount(newAmount);
+		bill1.setPaid(datePaid);
+		System.out.println("----------Bill 1------------");
+		System.out.println(bill1);
 	}
 }
--- src/DateMoneyBillJUnit.java
+++ src/DateMoneyBillJUnit.java
@@ -1,52 +1,52 @@
-//
-///*
-// * JUnit tests for Date, Money, and Bill classes
-// * @author Lesley Kalmin
-// *  CSS162
-// */
-//
-//import static org.junit.Assert.*;
-//
-//import org.junit.Test;
-//
-//public class DateMoneyBillJUnit {
-//
-//	@Test
-//	public void ConstructMoneyTest() {
-//		Money money1 = new Money(10);
-//		
-//		assertEquals(10, money1.getDollars());		
-//	}
-//	
-//	@Test
-//	public void SetMoneyTest()
-//	{
-//		Money money1 = new Money();
-//		
-//		money1.setMoney(30,50);
-//        assertEquals(30, money1.getDollars());      
-//        assertEquals(50, money1.getCents());
-//	}
-//	
-//	@Test
-//	public void ConstructMoneyCopyTest() {
-//		Money money1 = new Money();
-//		money1.setMoney(10,40);		
-//		
-//        Money money2 = new Money(money1);
-//        
-//        assertEquals(10, money1.getDollars());       
-//        assertEquals(40, money2.getCents());
-//	}
-//	
-//	@Test
-//	public void PrintMoneyTest()
-//	{
-//		Money money1 = new Money(10);
-//
-//        money1.setMoney(30,50);
-//        assertEquals("$30.50", money1.toString());
-//		
-//	}
-//
-//}
+
+/*
+ * JUnit tests for Date, Money, and Bill classes
+ * @author Lesley Kalmin
+ *  CSS162
+ */
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DateMoneyBillJUnit {
+
+	@Test
+	public void ConstructMoneyTest() {
+		Money money1 = new Money(10);
+		
+		assertEquals(10, money1.getDollars());		
+	}
+	
+	@Test
+	public void SetMoneyTest()
+	{
+		Money money1 = new Money();
+		
+		money1.setMoney(30,50);
+        assertEquals(30, money1.getDollars());      
+        assertEquals(50, money1.getCents());
+	}
+	
+	@Test
+	public void ConstructMoneyCopyTest() {
+		Money money1 = new Money();
+		money1.setMoney(10,40);		
+		
+        Money money2 = new Money(money1);
+        
+        assertEquals(10, money1.getDollars());       
+        assertEquals(40, money2.getCents());
+	}
+	
+	@Test
+	public void PrintMoneyTest()
+	{
+		Money money1 = new Money(10);
+
+        money1.setMoney(30,50);
+        assertEquals("$30.50", money1.toString());
+		
+	}
+
+}
