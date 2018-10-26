54fe54973487d55cbc8fb66784059c575d19d59b
Ian B <ianbryanz@gmail.com>
2018-10-25 17:17:02 -0700

    Documentation Added to Date Class
    Documentation according to proper Javadoc format has been added to each method in Date.

--- src/Date.java
+++ src/Date.java
@@ -1,22 +1,31 @@
+/**
+ * @author Ian Bryan
+ * @version 10/25/2018
+ *
+ */
 public class Date {
 
-	//class variables
-	private int day;
-	private int month;
-	private int year;
+	/**
+	 * Class Data Members
+	 * day | month | year
+	 * All integer values and constrained to general boundaries of the Gregorian calendar.
+	 * */
+	private int day; 
+	private int month; 
+	private int year; 
 
-	/*****************************************************/
-	/* This ctor will check for a few invariants		 */
-	/* The days have to be 1 < days < 31 while the months*/
-	/* must be 1 < months < 12 and the year between 	 */
-	/* 2014 and 2024. The class variables will only be 	 */
-	/* set if all these conditions are met true.		 */
-	/*****************************************************/
+	/**
+	 * @param month - {B/ween 1-31}
+	 * @param day - {B/ween 1-12}
+	 * @param year - {B/ween 2014-2024}
+	 * 
+	 * Constructor will take in three values of the date, then assign their values to the class data members if the conditions
+	 * are satisfied.
+	 */
 	public Date(int month, int day, int year){
 
 		if(day < 1 || day > 31){
 			System.out.println("invalid day: " + day);
-			//assert(day > 0);
 			System.exit(0);
 		}else if(month < 1 || month > 12){
 			System.out.println("invalid month: "+ month);
@@ -31,18 +40,21 @@ public class Date {
 		}
 	}
 
-	//copy ctor
+	
+	/**
+	 * @param other - {Copy constructor takes in object 'other' and replaces current values into the new instance of itself}
+	 */
 	public Date(Date other){
 		this.day = other.day;
 		this.month = other.month;
 		this.year = other.year;
 	}
 
-	/*****************************************************/
-	/* the isAfter method will return false under any	 */
-	/* condition unless the day of the handed date is GT */
-	/* the current one by as little as 1 day			 */
-	/*****************************************************/
+	
+	/**
+	 * @param compareToDate
+	 * @return boolean - {Always false unless handed date is greater by at least 1 day}
+	 */
 	public boolean isAfter(Date compareToDate){
 		if(compareToDate.day > day && compareToDate.month >= month
 				&& compareToDate.year >= year){
@@ -52,52 +64,62 @@ public class Date {
 		}
 	}
 
-	//checking for equivalence
+	/**
+	 * @param date - {Host for any object. In this case a Date object which 
+	 * has its values compared against the previous version of itself.}
+	 * */
 	public boolean equals(Object date){
 		//using the custom toString method on both
 		//objects is necessary to be able to compare them
 		//in the same format
 		if(this.toString().equals(date.toString())){
-			System.out.println("the two dates are the same");
+			//System.out.println("the two dates are the same");
 			return true;
 		}else{
-			System.out.println("the two dates are not the same");
+			//System.out.println("the two dates are not the same");
 			return false;
 		}
 	}
 
-	/*****************/
-	/* Begin getters */
-	/*****************/
-
+	/**
+	 * @return day - {representative of current object value for day}
+	 */
 	public int getDay(){
-		return day;
+		return this.day;
 	}
 
+	/**
+	 * @return month - {representative of current object value for month}
+	 */
 	public int getMonth(){
-		return month;
+		return this.month;
 	}
 
+	/**
+	 * @return year - {representative of current object value for year}
+	 */
 	public int getYear(){
-		return year;
+		return this.year;
 	}
 
-	/*****************************************************/
-	/* End getters, begin setters. These setters include */
-	/* the validation of the date when called from the	 */
-	/* BillMoneyDateDriver class and prevent illegal	 */
-	/* dates or the changing of dates to curb due dates	 */
-	/*****************************************************/
+	
+	/**
+	 * @param day
+	 * Takes in a day value to be assigned, if condition 1<X<31 is satisfied, then assign to the current object's day variable.
+	 */
 	public void setDay(int day){
 		if(day < 1 || day > 31){
 			System.out.println("invalid day " + day + "\nProgram Exiting...");
-			assert(day > 0);
 			System.exit(0);
 		}else{
 			this.day = day;	
 		}
 	}
 
+	/**
+	 * @param month
+	 * Takes in a month value, checks if the condition 1 < X < 12 is satisfied. If yes, then assign the value to the current object.
+	 */
 	public void setMonth(int month){
 		if(month < 1 || month > 12){
 			System.out.println("invalid month " + month + "\nProgram Exiting...");
@@ -109,22 +131,25 @@ public class Date {
 		}
 	}
 
+	/**
+	 * @param year
+	 * Takes in a year and checks that is satisfies the condition 2014 < X < 2024
+	 */
 	public void setYear(int year){
 		if(year < 2014 || year > 2024){
 			System.out.println("invalid year " + year + "\nProgram Exiting...");
-			assert(year < 2024);
-			assert(year > 0);
 			System.exit(0);
 		}else{
 			this.year = year;
 		}
 	}
-
-	/**************/
-	/* end setters*/
-	/**************/
-
+	
+	/**
+	 * Basic overriding toString() method that simply puts out the month, day, and year
+	 * separated by "/"'s
+	 * */
+	@Override
 	public String toString(){
-		return month + "/" + day + "/" + year;
+		return this.month + "/" + this.day + "/" + this.year;
 	}
 }
